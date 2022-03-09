7ac3ef1580cb1b3a665e584d74bac3ef0143a8a6
Chad Campbell <94390599+chadjcampbell@users.noreply.github.com>
2022-03-09 14:22:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import login_required
+from helpers import login_required, apology
 
 # Configure application
 app = Flask(__name__)
